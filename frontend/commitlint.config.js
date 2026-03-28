58e86f3abc5b0e00244e40f15eb46d33aff771e4
theWebCookie <jabwojtek@wp.pl>
2026-03-28 17:09:37 +0100

    setup

---
+++ frontend/commitlint.config.js
@@ -0,0 +1,41 @@
+﻿export default {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (commit) => commit.includes("chore(release):"),
+    (commit) => commit.includes("Merge "),
+    (commit) => commit.includes("(#") && commit.includes(")")
+  ],
+  plugins: [
+    {
+      rules: {
+        "custom-type-enum": (parsed) => {
+          const expectedTypes = [
+            "feat",
+            "fix",
+            "test",
+            "build",
+            "refactor",
+            "perf",
+            "docs",
+            "ci",
+            "chore",
+            "style",
+            "revert"
+          ];
+
+          if (!expectedTypes.includes(parsed.type)) {
+            return [
+              false,
+              `Invalid commit type. Must be one of: ${expectedTypes.join(", ")}.`
+            ];
+          }
+
+          return [true];
+        }
+      }
+    }
+  ],
+  rules: {
+    "custom-type-enum": [2, "always"]
+  }
+};
