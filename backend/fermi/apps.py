afb4cb3aa3712bf97da344681bbe04505db3ae9f
KajetanJanusz <134972496+KajetanJanusz@users.noreply.github.com>
2026-03-29 16:32:27 +0200

    Init django project (#5)
    * update
    
    * initialize Django project with basic structure and configurations
    
    * ruff
    
    * fix: update ALLOWED_HOSTS to allow all hosts during development

---
+++ backend/fermi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FermiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fermi"
--- entrypoints/backend.sh
+++ entrypoints/backend.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+python manage.py migrate
+python manage.py seed_data
+python manage.py runserver
