ea5b3cd98f9cc8c25aa489eeb63afada7434e859
kajetanjanusz <kajtek.k.janusz@gmail.com>
2026-04-11 22:55:09 +0200

    feat: register FermiSource model in admin and update entrypoint to run on all interfaces

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py seed_data_fermi
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
--- backend/fermi/admin.py
+++ backend/fermi/admin.py
@@ -1,1 +1,6 @@
 # Register your models here.
+from django.contrib import admin
+
+from fermi.models import FermiSource
+
+admin.site.register(FermiSource)
